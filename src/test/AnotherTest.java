30e598f8a8ddab1af1dbc9cf91103c394bdc9545
Ike Maldonado <maldonadoike@gmail.com>
2021-10-05 13:46:14 -0700

    checkpoint

--- src/test/AnotherTest.java
+++ src/test/AnotherTest.java
@@ -5,43 +5,53 @@ import utils.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class AnotherTest {
-        QueryStatements query = (sql, resultSetIterator) -> {
-            List<String> list = new ArrayList<>();
-            try {
 
-                JDBC.openConnection();
-
-                PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
-                ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                    list.add(rs.getString(resultSetIterator.iterator().next().toString()));
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-            return list;
-        };
-}
-
-class Thing {
     public static void main(String[] args) {
-
-        String sql = "SELECT * FROM customers";
-        ResultSetIterator resultSetIterator = new ResultSetIterator("Address", "Phone", "Customer_Name");
-        AnotherTest anotherTest = new AnotherTest();
-
-        List<String> myListOfQueryObjects = anotherTest.query.whileLoop(sql, resultSetIterator);
-
-        System.out.println(myListOfQueryObjects);
-
-
-
+        new ProcessResultSet(new GetResultSet().getResultSet("SELECT * FROM customers"));
 
     }
+
+    // need a method that consumes a QueryStatements Object
 }
+
+class GetResultSet implements GetResultSetInterface{
+    @Override
+    public ResultSet getResultSet(String sql) {
+        try {
+            PreparedStatement ps = JDBC.openConnection().prepareStatement(sql);
+            return ps.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+}
+
+class ProcessResultSet {
+    ProcessResultSet(ResultSet resultSet){
+        try {
+            while(resultSet.next()){
+                System.out.println(resultSet.getRow());
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
+
+@FunctionalInterface
+interface GetResultSetInterface{
+    // this is the expression contract
+    ResultSet getResultSet(String sql);
+}
+
+@FunctionalInterface
+interface ProcessResultSetInterface{
+    // processes the expression above
+    void processResultSet(GetResultSet getResultSet);
+}
+
+
 
--- src/utils/QueryStatements.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface QueryStatements {
-    // it already knows what type of object to pass into the method. No need to define it twice.
-    abstract List<String> whileLoop(String sql, ResultSetIterator resultSetIterator);
-}
