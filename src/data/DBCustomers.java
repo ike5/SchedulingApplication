8783af042b422c816149bf50616fa505cf3be455
Ike Maldonado <maldonadoike@gmail.com>
2021-10-09 10:04:46 -0700

    having issues with DBCustomer's class must review

--- src/data/DBCustomers.java
+++ src/data/DBCustomers.java
@@ -5,46 +5,145 @@ import javafx.collections.ObservableList;
 import model.Customer;
 import utils.ProcessQuery;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 //TODO
 // - Add CRUD functionality
 // - remove extending JDBC
 
 
+public class DBCustomers {
 
-public class DBCustomers extends JDBC {
-    public Customer addCustomer() {
+    public Customer addCustomer(String customerName, String address, String postalCode, String phone, int divisionId) {
+        String sql = "INSERT INTO customers (Customer_Name, Address, Postal_Code, Phone, Division_ID) VALUES (" +
+                "'" + customerName + "', " +
+                "'" + address + "', " +
+                "'" + postalCode + "', " +
+                "'" + phone + "', " + divisionId + ")";
+        Customer customer = null;
+        try {
+            PreparedStatement ps = JDBC.openConnection().prepareStatement(sql);
+            int sentinelValue = ps.executeUpdate();
+            ResultSet resultSet = getCustomerResultSet(sentinelValue);
+            while(resultSet.next()){
+                System.out.println(
+                        "Customer_ID: " + resultSet.getInt(1) +
+                        "\t Customer_Name: " + resultSet.getString(2) +
+                        "\t Address: "+ resultSet.getString(3)
+                );
+            }
+            return customer;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return customer;
+    }
+
+    public static void main(String[] args) {
+        JDBC.openConnection();
+        System.out.println(
+                new DBCustomers().addCustomer("John Maloney", "321 Main Street", "93342", "990-998-5434", 323)
+        );
+    }
+
+    /**
+     * Returns the ResultSet object of all customers in the customer database table.
+     *
+     * @return ResultSet object or null if query unsuccessful or if table empty
+     */
+    public ResultSet getAllCustomersResultSet() {
+        String sql = "SELECT * FROM customers INNER JOIN first_level_divisions ON first_level_divisions.Division_ID = customers.Division_ID";
+        try {
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            return ps.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
+    public ResultSet getCustomerResultSet(int customerId){
+        String sql = "SELECT * FROM customers WHERE Customer_ID = " + customerId;
+        try {
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            return ps.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Returns an ObservableList<Customer> object of all customers in the customer database table.
+     *
+     * @return an ObservableList<Customer> object or null if no entries.
+     */
     public ObservableList<Customer> getAllCustomers() {
         ObservableList<Customer> customerList = FXCollections.observableArrayList();
-        // widen query SELECT *
-        // select * from customers INNER JOIN first_level_divisions ON first_level_divisions.Division_ID = customers.Division_ID;
-        ProcessQuery.processExecute("select * from customers INNER JOIN first_level_divisions ON first_level_divisions.Division_ID = customers.Division_ID", resultSet -> {
-            Customer customer = new Customer(
-                    resultSet.getInt("Customer_ID"),
-                    resultSet.getString("Customer_Name"),
-                    resultSet.getString("Address"),
-                    resultSet.getString("Postal_Code"),
-                    resultSet.getString("Phone"),
-                    resultSet.getInt("Division_ID"),
-                    resultSet.getInt("COUNTRY_ID")
-            );
-            customerList.add(customer);
-        });
-        return customerList;
-    }
-
-    public Customer getCustomer(int customerId) {
+        try {
+            ResultSet resultSet = getAllCustomersResultSet(); // helper method
+            while (resultSet.next()) {
+                Customer customer = new Customer(
+                        resultSet.getInt("Customer_ID"),
+                        resultSet.getString("Customer_Name"),
+                        resultSet.getString("Address"),
+                        resultSet.getString("Postal_Code"),
+                        resultSet.getString("Phone"),
+                        resultSet.getInt("Division_ID"),
+                        resultSet.getInt("COUNTRY_ID")
+                );
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
+    /**
+     * Returns a Customer object provided a customer ID.
+     *
+     * @param customerId An integer representing the customer ID
+     * @return Customer object or null if no such customer exists
+     */
+    public Customer getCustomer(int customerId) {
+        String sql = "SELECT * FROM customers WHERE Customer_ID = " + customerId;
+        Customer customer = null;
+        try {
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                customer = new Customer(
+                        resultSet.getInt("Customer_ID"),
+                        resultSet.getString("Customer_Name"),
+                        resultSet.getString("Address"),
+                        resultSet.getString("Postal_Code"),
+                        resultSet.getString("Phone"),
+                        resultSet.getInt("Division_ID"),
+                        resultSet.getInt("COUNTRY_ID")
+                );
+            }
+            return customer;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return customer;
+    }
 
 
-    // FIXME
-    //
-    public Customer editCustomer(Customer customer) {
-//        AtomicReference<Customer> C = null;
+    /**
+     * Updates a customer entry in the customers database table provided a Customer object.
+     *
+     * @param customer A Customer object
+     * @return The updated Customer object reflecting what's in the database after the change or null if
+     * the update was unsuccessful.
+     */
+    public Customer updateCustomer(Customer customer) {
         String sql = "UPDATE customers SET" +
                 " Customer_Name = " + customer.getName() +
                 ", Address = " + customer.getAddress() +
@@ -53,24 +152,22 @@ public class DBCustomers extends JDBC {
                 ", Division_ID = " + customer.getDivisionID() +
                 " WHERE Customer_ID = " + customer.getId();
 
-//        ProcessQuery.process(sql, resultSet -> {
-//            assert false;
-//            C.set(new Customer(
-//                    resultSet.getInt("Customer_ID"),
-//                    resultSet.getString("Customer_Name"),
-//                    resultSet.getString("Address"),
-//                    resultSet.getString("Postal_Code"),
-//                    resultSet.getString("Phone"),
-//                    resultSet.getInt("Division_ID")
-//            ));
-//
-//        });
+        try {
+            PreparedStatement ps = JDBC.openConnection().prepareStatement(sql);
+            int sentinelValue = ps.executeUpdate();
+
+            return getCustomer(sentinelValue);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
     public boolean deleteCustomer(Customer customer) {
         return false;
     }
+
+
 
 
 }
---
+++ src/test/TestCustomer.java
@@ -0,0 +1,10 @@
+package test;
+
+import model.Customer;
+
+public class TestCustomer {
+    public static Customer customer;
+
+    public TestCustomer(){
+    }
+}
