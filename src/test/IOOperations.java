9b3007b46fc58af3e6dfaa7509f2a6f8ea4d6cd9
Ike Maldonado <maldonadoike@gmail.com>
2021-11-08 08:00:58 -0800

    file making tests

--- src/test/IOOperations.java
+++ src/test/IOOperations.java
@@ -13,37 +13,49 @@ public File(String parent, String child)
  */
     void copyStream(InputStream in, OutputStream out) throws IOException {
         int b;
-        while((b = in.read()) != -1){
+        while ((b = in.read()) != -1) {
             out.write(b);
         }
     }
 
-    void copyStream(Reader in, Writer out)throws IOException{
+    void copyStream(Reader in, Writer out) throws IOException {
         int b;
-        while((b = in.read()) != -1){
+        while ((b = in.read()) != -1) {
             out.write(b);
         }
     }
 
-    public void printData(InputStream is) throws IOException{
+    public void printData(InputStream is) throws IOException {
         int b;
         StringBuilder sb = new StringBuilder();
-        while((b = is.read()) != -1){
+        while ((b = is.read()) != -1) {
             System.out.println(sb.append(b));
         }
     }
 
-    public void readFile(String fileName) throws IOException{
-        try(var fis = new FileInputStream(fileName)){
+    public void readFile(String fileName) throws IOException {
+        try (var fis = new FileInputStream(fileName)) {
             printData(fis);
         }
     }
 
     void copyTextFile(File src, File dest) throws IOException {
-        try(var reader = new FileReader(src); var writer = new FileWriter(dest)){
+        try (var reader = new FileReader(src); var writer = new FileWriter(dest)) {
             int b;
-            while((b = reader.read()) != -1){
+            while ((b = reader.read()) != -1) {
                 writer.write(b);
+                writer.write("#");
+            }
+        }
+    }
+
+    void copyTextWithBuffer(File src, File dest) throws IOException {
+        try (var reader = new BufferedReader(new FileReader(src));
+             var writer = new BufferedWriter(new FileWriter(dest))) {
+            String s;
+            while((s = reader.readLine()) != null){
+                writer.write(s.toUpperCase());
+                writer.newLine();
             }
         }
     }
@@ -52,7 +64,7 @@ public File(String parent, String child)
         File fileName = new File("src/data/login_tracker.txt");
         File fileName2 = new File("src/data/another.txt");
 
-        new IOOperations().copyTextFile(fileName, fileName2);
+        new IOOperations().copyTextWithBuffer(fileName, fileName2);
     }
 
 
