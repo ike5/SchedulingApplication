529c346a0863f9fde7ab481e0b35307991c99d63
Ike Maldonado <maldonadoike@gmail.com>
2021-11-29 18:26:19 -0800

    add documentation to appointments controller

--- src/controller/AppointmentsController.java
+++ src/controller/AppointmentsController.java
@@ -3,6 +3,7 @@ package controller;
 import data.DBAppointment;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -20,6 +21,10 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+/**
+ * This class displays appointments to the user by either month, week, or all time. New appointments can be added,
+ * and existing appointments can be modified.
+ */
 public class AppointmentsController implements Initializable {
     public TableView table_view_id;
     public TableColumn appointment_id_tablecolumn;
@@ -40,6 +45,13 @@ public class AppointmentsController implements Initializable {
     public Button delete_button;
     public Button update_button;
 
+    /**
+     * Initializes TableView with Appointment object. This method also ensures that Delete and Update buttons
+     * start off inactive until a table item is selected.
+     *
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -56,6 +68,7 @@ public class AppointmentsController implements Initializable {
         customer_id_tablecolumn.setCellValueFactory(new PropertyValueFactory<Appointment, Integer>("CustomerId"));
         user_id_tablecolumn.setCellValueFactory(new PropertyValueFactory<Appointment, Integer>("UserId"));
 
+        // TableView listener
         table_view_id.getSelectionModel().selectedItemProperty().addListener((observable, oldSelection, newSelection) -> {
             if (newSelection != null) {
                 // Enable buttons if row selected
@@ -70,36 +83,81 @@ public class AppointmentsController implements Initializable {
         update_button.setDisable(table_view_id.getSelectionModel().isEmpty());
     }
 
+    /**
+     * Switches user to the CustomersController screen.
+     *
+     * @param actionEvent Back Button pressed
+     * @throws IOException
+     */
+    @FXML
     public void backButtonOnAction(ActionEvent actionEvent) throws IOException {
-        switchView(actionEvent, "/view/Customers.fxml", "Hello");
+        switchView(actionEvent, Main.resourceBundle.getString("customers_screen"), "Hello");
     }
 
+    /**
+     * Sets the TableView to display all appointments in month.
+     *
+     * @param actionEvent Month RadioButton
+     */
     public void monthViewRadioButtonOnAction(ActionEvent actionEvent) {
         table_view_id.setItems(DBAppointment.getAllAppointmentsInMonth());
     }
 
+    /**
+     * Sets the TableView to display all appointments in week.
+     *
+     * @param actionEvent Week RadioButton
+     */
     public void weekViewRadioButtonOnAction(ActionEvent actionEvent) {
         table_view_id.setItems(DBAppointment.getAllAppointmentsInWeek());
     }
 
+    /**
+     * Sets the TableView to display all available appointments.
+     *
+     * @param actionEvent All Appointments RadioButton
+     */
     public void allAppointmentsRadioButtonOnAction(ActionEvent actionEvent) {
         table_view_id.setItems(DBAppointment.getAllAppointments());
     }
 
+    /**
+     * Switches user to the ModifyAppointmentController screen. This
+     * method sets the Appointment singleton to null before switching
+     * views.
+     *
+     * @param actionEvent New Appointment Button pressed
+     * @throws IOException
+     */
     public void newAppointmentButtonOnAction(ActionEvent actionEvent) throws IOException {
         AppointmentSingleton.getInstance().setAppointment(null);
+
         switchView(actionEvent, Main.resourceBundle.getString("modify_appointment_screen"), "New Appointment");
     }
 
+    /**
+     * Switches user to the ModifyAppointmentController screen. This
+     * method captures the Appointment object with the Appointment
+     * singleton before switching views.
+     *
+     * @param actionEvent Update Appointment Button is pressed
+     * @throws IOException
+     */
     public void updateAppointmentButtonOnAction(ActionEvent actionEvent) throws IOException {
         if (table_view_id.getSelectionModel().selectedItemProperty() != null) {
             AppointmentSingleton.getInstance().setAppointment((Appointment) table_view_id.getSelectionModel().getSelectedItem());
+
             switchView(actionEvent, Main.resourceBundle.getString("modify_appointment_screen"), "Modify Appointment");
         } else {
             Messages.errorMessage("Please select an appointment", "Nothing selected");
         }
     }
 
+    /**
+     * Deletes a selected appointment.
+     *
+     * @param actionEvent Delete Appointment Button is pressed
+     */
     public void deleteAppointmentButtonOnAction(ActionEvent actionEvent) {
         AppointmentSingleton.getInstance().setAppointment((Appointment) table_view_id.getSelectionModel().getSelectedItem());
 
@@ -107,10 +165,22 @@ public class AppointmentsController implements Initializable {
         if (result.isPresent() && result.get() == ButtonType.OK) {
             DBAppointment.deleteAppointment(AppointmentSingleton.getInstance().getAppointment().getAppointmentId());
         }
+
+        // After deleting item, disable button until another item is selected.
         delete_button.setDisable(true);
+
+        // Repopulate TableView with new list of appointments
         table_view_id.setItems(DBAppointment.getAllAppointments());
     }
 
+    /**
+     * Helper method that simplifies the view switching process.
+     *
+     * @param actionEvent The ActionEvent from the parent method
+     * @param path        The new View to switch to
+     * @param title       The title of the new View
+     * @throws IOException
+     */
     private void switchView(ActionEvent actionEvent, String path, String title) throws IOException {
         Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
         Parent scene = FXMLLoader.load(getClass().getResource(path));
